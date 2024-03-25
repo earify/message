d5a3313223be276a94f5174a1943aae3d2556968
earify <112871717+earify@users.noreply.github.com>
2024-03-25 10:00:44 +0900

    3개로 나눔

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+var firebaseConfig = {
   // 복사한 Firebase 설정을 여기에 붙여넣습니다.
   apiKey: "AIzaSyA98iMqdjl_2gD_TPPTU5kUkqTkGQLypus",
   authDomain: "msgify-fa5b6.firebaseapp.com",
@@ -11,34 +11,33 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-
 const messagesRef = firebase.database().ref('messages');
 
 function sendMessage() {
-    const nickname = document.getElementById('nickname').value.trim();
-    const message = document.getElementById('message').value.trim();
+  const nickname = document.getElementById('nickname').value.trim();
+  const message = document.getElementById('message').value.trim();
 
-    if (nickname && message) {
-        messagesRef.push({ nickname, message });
-        document.getElementById('message').value = ''; // 메시지 필드 초기화
-    }
+  if (nickname && message) {
+      messagesRef.push({ nickname, message });
+      document.getElementById('message').value = ''; // 메시지 필드 초기화
+  }
 }
 
 document.getElementById('message').addEventListener('keydown', function(event) {
-    if (event.shiftKey && event.key === 'Enter') {
-        event.preventDefault();
-        this.value += "\n";
-    } else if (event.key === 'Enter') {
-        event.preventDefault();
-        sendMessage();
-    }
+  if (event.shiftKey && event.key === 'Enter') {
+      event.preventDefault();
+      this.value += "\n";
+  } else if (event.key === 'Enter') {
+      event.preventDefault();
+      sendMessage();
+  }
 });
 
 messagesRef.on('child_added', function(snapshot) {
-    const message = snapshot.val();
-    const messageElement = document.createElement('div');
-    messageElement.textContent = `${message.nickname}: ${message.message}`;
-    document.getElementById('messages').appendChild(messageElement);
+  const message = snapshot.val();
+  const messageElement = document.createElement('div');
+  messageElement.textContent = `${message.nickname}: ${message.message}`;
+  document.getElementById('messages').appendChild(messageElement);
 
-    document.getElementById('messages').scrollTop = document.getElementById('messages').scrollHeight;
+  document.getElementById('messages').scrollTop = document.getElementById('messages').scrollHeight;
 });
